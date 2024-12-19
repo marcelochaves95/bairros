32b4d48a1071af840c03cc92ab063dca78f8a7b4
Marcelo Chaves <marcelo.chaves@wildlifestudios.com>
2024-12-19 02:59:37 -0300

    Removed license

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8")
